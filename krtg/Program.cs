e69c63750636718ffa2a84b895604a0bbf7440c4
Андрей Гребёнкин <andrey_greb03@mail.ru>
2021-10-11 10:12:04 +0300

    Добавьте файлы проекта.

---
+++ krtg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace krtg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple = GetValues();
+            Console.WriteLine(tuple.Item1); // 1
+            Console.WriteLine(tuple.Item2); // 3
+            Console.WriteLine(tuple.Item3);
+            Console.WriteLine(tuple.Item4);
+
+            Console.Read();
+        }
+        private static (int, int, string, string) GetValues()
+        {
+            var result = (1, 3, "Привет", ":)");
+            return result;
+        }
+    }
+}
